15200c6b4801304961408578751656993065f66e
mounamouna <namuiiimouna01@gmail.com>
2025-07-31 12:54:45 -1200

    AffichageProduit(3eme commit)

--- src/Pages/PageAccueil/Acceuil.jsx
+++ src/Pages/PageAccueil/Acceuil.jsx
@@ -1,24 +1,52 @@
-import { Link, useNavigate } from "react-router-dom";
-import "./acceuil.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Navbar from "../../Components/Nav_Bar/Navbar";
+import "./acceuil.css";
 
 const Accueil = () => {
-  const navigate = useNavigate();
+  const [produits, setProduits] = useState([]);
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
-  };
+  useEffect(() => {
+    const fetchProduits = async () => {
+      try {
+        const token = localStorage.getItem("token"); 
+        const response = await axios.get("http://localhost:5000/api/products", {
+          headers: {
+            Authorization: `Bearer ${token}`, 
+          },
+        });
+
+        setProduits(response.data.data);
+      } catch (error) {
+        console.error("Erreur lors du chargement des produits : ", error);
+      }
+    };
+
+    fetchProduits();
+  }, []);
 
   return (
-    <div>
-      <Navbar />
+  <div>
+    <Navbar />
+    <div className="content">
+      
 
-      <div className="content">
-        <h1>Bienvenue sur la page d’accueil</h1>
+      <h2 className="section-title">Produits en vente</h2>
+      <div className="horizontal-scroll">
+        {produits.map((produit) => (
+          <div key={produit.id} className="horizontal-card">
+            {produit.image_url && (
+              <img src={produit.image_url} alt={produit.name} />
+            )}
+            <h3>{produit.name}</h3>
+            <p className="prix">{produit.price} DA</p>
+            <button>Acheter</button>
+          </div>
+        ))}
       </div>
     </div>
-  );
-};
+  </div>
+);
+}
 
 export default Accueil;
